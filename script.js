99bbcc363fc102e11889f77c057c29ae62fb60f3
Alistair Houghton <alistair.g.houghton@gmail.com>
2021-11-15 17:59:01 +0000

    updated the script file

--- script.js
+++ script.js
@@ -1,5 +1,30 @@
 const currentDay = document.getElementById('currentDay')
 const m = moment();
-console.log(m.toString())
+const timerContainerValue = document.getElementById(timecontainerid);
+var currentTime = m.format("hh");
+var timeValue = document.getElementById('timeValue');
+var inputArea = document.getElementById("eventText");
+var trueTimeValue = timeValue.innerHTML;
 
-currentDay.textContent = m.toString()
+// ====================================================================
+
+console.log(currentTime);
+console.log(trueTimeValue);
+
+// foreachloop? ====================================
+
+if (trueTimeValue == currentTime){
+    inputArea.classList.add('currentTime');
+}
+else if (trueTimeValue > currentTime) {
+    inputArea.classList.add('futureTime');
+}
+else {
+    inputArea.classList.add('pastTime');
+}
+
+// ============================================================================
+
+currentDay.textContent = m.format('MMMM Do YYYY, h:mm:ss a');
+
+
