f7427e48be73409b0877a2b791198584e4400175
Alistair Houghton <alistair.g.houghton@gmail.com>
2021-11-17 15:28:21 +0000

    updated changes

--- script.js
+++ script.js
@@ -3,16 +3,68 @@ const m = moment();
 var inputArea = document.querySelectorAll(".text-area");
 var currentHour = moment().hour();
 var currentTime = m.format("HH");
-// var trueTimeValue = timeValue.innerHTML;
-// var timeValue = document.getElementById('timeValue')
+var timeValue = document.querySelector('.timeValue')
+var trueTimeValue = timeValue.innerHTML;
+
+// vars ==================================================================================
+
+function timeCheck() {  
+    const textAreaCollection = document.querySelectorAll('#text-area-class');
+    
+    textAreaCollection.forEach(element => {
+        if (currentTime== trueTimeValue){
+            element.classList.add('currentTime');
+            element.classList.remove('pastTime');
+        }
+        else if (currentTime > trueTimeValue) {
+            element.classList.add('futureTime');
+            element.classList.remove('currentTime');
+        }
+        else {
+            element.classList.add('pastTime');
+            element.classList.remove('futuretime')
+        }
+    });
+}
+
+timeCheck()
+
+console.log('current hour', currentHour);
+console.log('current time', currentTime);
+console.log('truetimevalue', trueTimeValue);
+
+currentDay.textContent = m.format('MMMM Do YYYY, h:mm:ss a');
+
+// save function ====================================================================
+
+$('.saveBtn').on('click',function() {
+
+var textInput = $(this).siblings('.text-area').val();
+var timeText = $(this).parent().attr('id');
+
+console.log('textinput ',textInput);
+console.log('time ',timeText);
+
+localStorage.setItem(timeText, textInput);
+
+
+
+});
+
+
+// var currentHour = '10'
+// var currentTime = '09'
+// console.log('timev',timeValue);
+// console.log('ttv',trueTimeValue);
 
 
 // var timeValue = document.querySelector('.timeValue');
 
 // const timerContainerValue = document.querySelector(timer-container);
-// console.log('timeValue', timeValue);
 
-
+// console.log('current hour', currentHour);
+// console.log('current time', currentTime);
+// console.log('truetimevalue', trueTimeValue);
 
 
 // ====================================================================
@@ -32,26 +84,9 @@ var currentTime = m.format("HH");
 // need a var of current hour which is moment().hour(). For each function, 
 
 
-
-function timeCheck() {  
 // $('#text-area-class').each (function() {
-const textAreaCollection = document.querySelectorAll('#text-area-class');
 
-textAreaCollection.forEach(element => {
-    if (currentHour == currentTime){
-                    element.classList.add('currentTime');
-                    element.classList.remove('pastTime');
-                }
-                else if (currentHour < currentTime) {
-                    element.classList.add('futureTime');
-                    element.classList.remove('currentTime');
-                }
-                else {
-                    element.classList.add('pastTime');
-                    element.classList.remove('futuretime')
-                }
-});
-// console.log('#text-area-class');
+            // console.log('#text-area-class');
 
 //         if (currentHour == currentTime){
 //             inputArea.classList.add('currentTime');
@@ -63,13 +98,6 @@ textAreaCollection.forEach(element => {
 //             inputArea.classList.add('pastTime');
 //         }
 //     })
-console.log('current hour', currentHour);
-console.log('current time', currentTime);
-
-}
-
-timeCheck()
-
 
 // if (trueTimeValue == currentTime){
 //     inputArea.classList.add('currentTime');
@@ -82,20 +110,3 @@ timeCheck()
 // }
 // ============================================================================
 
-currentDay.textContent = m.format('MMMM Do YYYY, h:mm:ss a');
-
-// save function ====================================================================
-
-$('.saveBtn').on('click',function() {
- 
-var textInput = $(this).siblings('.text-area').val();
-var timeText = $(this).parent().attr('id');
-
-console.log('textinput ',textInput);
-console.log('time ',timeText);
-
-localStorage.setItem(timeText, textInput);
-
-
-
-});
