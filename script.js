720d3238cb1dda12f6a587143de5f4daab240c6f
Alistair Houghton <alistair.g.houghton@gmail.com>
2021-11-20 19:35:56 +0000

    added a clear storage function

--- script.js
+++ script.js
@@ -1,83 +1,64 @@
-// var storedData = localStorage.getItem('storedText');
-
-// function getFromStorage () {
-
-//     const dataRecords = document.querySelectorAll('.timecard');
-
-//     dataRecords.forEach(element => {
-//     var storedText  = element.dataset.id
-    
-// });
-// }
-
-// getFromStorage()
-
-// ==================================================================================================
+// ==================================================================================
 
 function getFromStorage() {
-    const dataRecords = document.querySelectorAll('.timecard');
-    dataRecords.forEach((element) => {
-      var id = element.dataset.id;
-      var text = localStorage.getItem(id);
-      element.children[1].textContent = text;
-    });
-  }
+  const dataRecords = document.querySelectorAll(".timecard");
+  dataRecords.forEach((element) => {
+    var id = element.dataset.id;
+    var text = localStorage.getItem(id);
+    element.children[1].textContent = text;
+  });
+}
 
-  getFromStorage()
+getFromStorage();
 
-// ================================================================================================
+// =================================================================================
 
-const currentDay = document.getElementById('currentDay');
-var timeValue = document.querySelector('.timeValue');
+const currentDay = document.getElementById("currentDay");
+var timeValue = document.querySelector(".timeValue");
 var userHour = moment().hour();
 const m = moment();
 
-// var trueTimeValue = timeValue.innerHTML;
+// vars =============================================================================
 
-// vars ==================================================================================
+currentDay.textContent = m.format("MMMM Do YYYY, h:mm:ss a");
 
-currentDay.textContent = m.format('MMMM Do YYYY, h:mm:ss a');
+// timecard loop ====================================================================
 
-// timecard loop ============================================================================
+function timeCheck() {
+  const timeCardCollection = document.querySelectorAll(".timecard");
 
-function timeCheck() {  
-    const timeCardCollection = document.querySelectorAll('.timecard')
-    
-    timeCardCollection.forEach(element => {
-        
-        var blockTime = element.id;
-        var inputArea = element.querySelector('.text-area');
-        
-        
-        if (userHour == blockTime){
-            inputArea.classList.add('currentTime');
-            
-        }
-        else if (userHour < blockTime){
-            inputArea.classList.add('futureTime');
-            
-        }
-        else {
-            inputArea.classList.add('pastTime');
-            
-        }
-    });
+  timeCardCollection.forEach((element) => {
+    var blockTime = element.id;
+    var inputArea = element.querySelector(".text-area");
+
+    if (userHour == blockTime) {
+      inputArea.classList.add("currentTime");
+    } else if (userHour < blockTime) {
+      inputArea.classList.add("futureTime");
+    } else {
+      inputArea.classList.add("pastTime");
+    }
+  });
 }
 
-timeCheck()
+timeCheck();
 
-// ========================================================================================
+
 // save function ====================================================================
 
-$('.saveBtn').on('click', function () {
-    var textInput = $(this).siblings('.text-area').val();
-    var timeText = $(this).parent().data('id');
-    // console.log('textInput ',textInput);
-    // console.log('time ',timeText);
-    localStorage.setItem(timeText, textInput);
-  });
+$(".saveBtn").on("click", function () {
+  var textInput = $(this).siblings(".text-area").val();
+  var timeText = $(this).parent().data("id");
+  // console.log('textInput ',textInput);
+  // console.log('time ',timeText);
+  localStorage.setItem(timeText, textInput);
+});
 
+// ===================================================================================
 
+// clear storage =======================================================================
 
-// =============================================================================================
-
+$('.clearbtn').on('click', function () {
+  localStorage.clear()
+  window.location.reload();
+})
